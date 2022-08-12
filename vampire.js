e2a305294308c9beb04552ffa0bdcc866257ab09
Eds-Dbug <edsdbug@gmail.com>
2022-08-12 01:55:41 +0000

    Did all the vampires except stretch

--- vampire.js
+++ vampire.js
@@ -1,3 +1,5 @@
+const common = require("mocha/lib/interfaces/common");
+
 class Vampire {
   constructor(name, yearConverted) {
     this.name = name;
@@ -10,22 +12,32 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    //need something to swap this with creator
+    //somehow need to get creators creator if there is one
+    let currentVampire = this;
+    let numberOfVampires = 0;
 
+    while(currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires ++;
+    }
+    return numberOfVampires
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal ? true : false;
   }
 
   /** Stretch **/
@@ -36,7 +48,44 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    let currentVampire = this;
+    let otherVampire = vampire;
+    let currentVampireFromAncestor = currentVampire.numberOfVampiresFromOriginal;
+    let otherVampireFromAncestor = otherVampire.numberOfVampiresFromOriginal;
 
+    //console.log(currentVampire)
+
+   if(currentVampire.name === 'root') {
+    return currentVampire;
+   }
+   if(otherVampire.name === 'root') {
+    return otherVampire;
+   }
+
+    //so loop through the ammount of ancestors we need for this vampire for each one loop through the ammount needed for other vampire 
+
+
+    for(let i = 0; i < currentVampireFromAncestor; i++) {
+      if(currentVampire.creator.name === otherVampire.creator.name) {
+        return currentVampire.creator;
+      }
+      
+     
+      currentVampire = currentVampire.creator;
+      otherVampire = otherVampire.creator;
+    }
+
+    for(let j = 0; j < otherVampireFromAncestor; j++) {
+      //check if current
+
+      if(currentVampire.creator.name === otherVampire.creator.name) {
+        return currentVampire.creator;
+      }
+      
+      
+      currentVampire = currentVampire.creator;
+      otherVampire = otherVampire.creator;
+    }
   }
 }
 
